a8bb70b0ff7bef5ddc39b1fd799ec6e0c09610e8
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-09-09 23:45:35 +0100

    cor blue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,20 +4,20 @@ import {Nav,Navbar} from "reactstrap";
 const Header=()=>{
 
     return(
-        <Navbar container="sm" color="dark" dark>
+        <Navbar container="sm" color="primary" primary>
             <Link href="/" passHref>
-            <a className="navbar-brand">
+            <a className="navbar-brand text-white fw-bold">
                 Inicio
             </a>
             </Link>
-            <Nav className="flex-row" navbar>
+            <Nav className="flex-row gap-32" navbar>
                 <Link href="/products" passHref>
-                    <a className="nav-link me-2">
+                    <a className="nav-link me-2 text-white fw-bold">
                         Produtos
                     </a>
                 </Link>
                 <Link href="/cart" passHref>
-                    <a className="nav-link me-2">
+                    <a className="nav-link me-2 text-white fw-bold">
                         Carrinho
                     </a>
                 </Link>
